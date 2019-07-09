8ff23b04b82f79c3feb8843b9a08398224b11dc7
NathanielYoung <young.nathaniel95@gmail.com>
2019-07-09 13:58:25 -0600

    heroku fix

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
-
+const port = process.env.PORT || 3000
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello')
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 })
+app.listen(port);
 
-app.listen(3000, () => console.log('server running'))
+console.log("server started");
